622e4d18c1d37da6bc1db30df3bbfc97b17ad192
yuxiaokang-source <59059267+yuxiaokang-source@users.noreply.github.com>
2024-04-05 08:15:08 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(name = 'DESCtorch',
 			long_description = 'DESC is an unsupervised deep learning algorithm for clustering scRNA-seq data. The algorithm constructs a non-linear mapping function from the original scRNA-seq data space to a low-dimensional feature space by iteratively learning cluster-specific gene expression representation and cluster assignment based on a deep neural network. This iterative procedure moves each cell to its nearest cluster, balances biological and technical differences between clusters, and reduces the influence of batch effect. DESC also enables soft clustering by assigning cluster-specific probabilities to each cell, which facilitates the identification of cells clustered with high-confidence and interpretation of results.',
 			classifiers = [
         'Development Status :: 3 - Alpha',
-        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.8',
       	],
 			url = 'https://github.com/yuxiaokang-source/DESCtorch',
 			author = 'Xiaokang Yu,Xiangjie Li',
@@ -16,11 +16,8 @@ setup(name = 'DESCtorch',
 			include_package_data=True,
 			install_requires = [
 				'torch==1.10.2',
-				'scanpy==1.7.2',
-				'python-igraph==0.9.10',
-				'louvain==0.7.0',
-                'numba==0.51.2',
-                'umap-learn==0.4.6',
+				'scanpy==1.9.2',
+				'louvain==0.8.0',
 				'cytoolz==0.11.2'
 				],
 			zip_safe = False
